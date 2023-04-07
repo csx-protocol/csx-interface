f91825f1d89d71d72fbd0ee2ba8b5c6d06417510
charliedevcode <charliedevcode@gmail.com>
2023-04-07 15:51:56 +0200

    init

---
+++ src/app/shared/item.interface.ts
@@ -0,0 +1,28 @@
+export interface Item {
+    contractAddress: string;
+    seller: string;
+    sellerTradeUrl: [partner: string, token: string];
+    buyer: string;
+    buyerTradeUrl: [partner: string, token: string];
+    itemMarketName: string;
+    inspectLink: string;
+    itemImageUrl: string;
+    weiPrice: string;
+    averageSellerDeliveryTime: string;
+    float: [max: number, min: number, value: number];
+    status: string;
+    stickers: any[];
+    weaponType: string;
+    etherPrice: string;
+    trimmedAddress: string;
+    indexInfo: {
+        index: string;
+        weiPrice: string;
+        priceType: string;
+        weaponType: string;
+        itemMarketName: string;
+        nextIndex: string;
+        etherPrice: string;
+        priceInUSD: number;
+    };
+}
