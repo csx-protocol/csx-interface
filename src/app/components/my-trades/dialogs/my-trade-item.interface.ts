ad559644b4ed79c305e5156f9a54b1d5f45b1d83
charliedevcode <charliedevcode@gmail.com>
2023-04-07 19:45:56 +0200

    init

---
+++ src/app/components/my-trades/dialogs/my-trade-item.interface.ts
@@ -0,0 +1,34 @@
+import { TradeStatus } from "../my-trades.component";
+
+export interface MyTradeItem {
+    contractAddress: string;
+    seller: string;
+    sellerTradeUrl: [partner: string, token: string];
+    buyer: string;
+    buyerTradeUrl: [partner: string, token: string];
+    itemMarketName: string;
+    inspectLink: string;
+    itemImageUrl: string;
+    weiPrice: string;
+    averageSellerDeliveryTime: string;
+    float: [max: number, min: number, value: number];
+    status: string;
+    stickers: Sticker[];
+    weaponType: string;
+    etherPrice: string;
+    trimmedAddress: string;
+    uiInfo: UiInfo;
+  }
+  
+  interface Sticker {
+    name: string;
+    category: string;
+    index: string;
+    imageUrl: string;
+  }
+  
+  interface UiInfo {
+    contractAddress: string;
+    role: string;
+    status: string;
+  }
