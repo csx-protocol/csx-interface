dd80594acad0beb629c6df3d255a84408b392ad5
charliedevcode <charliedevcode@gmail.com>
2023-04-08 11:24:03 +0200

    dialogs

--- src/app/components/my-trades/dialog/dialog.component.ts
+++ src/app/components/my-trades/dialog/dialog.component.ts
@@ -1,23 +1,18 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MyTradeItem } from '../my-trade-item.interface';
+import { MyTradeItem } from '../dialogs/my-trade-item.interface';
 import { Web3Service } from 'src/app/shared/web3.service';
-enum Role {
-  BUYER = 'Buyer',
-  SELLER = 'Seller',
-}
+
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-
 export class DialogComponent {
   item: MyTradeItem;
   constructor(public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any, private web3: Web3Service) {
-    console.log('data', data);
-
+    // console.log('data', data);
     this.item = this.initDialog(data);
   }
 
