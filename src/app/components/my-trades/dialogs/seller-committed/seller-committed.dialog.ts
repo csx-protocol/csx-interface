05f8af0228e90af3b42764a5b6a218f2f5b716c7
charliedevcode <charliedevcode@gmail.com>
2023-04-08 11:28:12 +0200

    removed unnessecary MatDialogRef

--- src/app/components/my-trades/dialogs/seller-committed/seller-committed.dialog.ts
+++ src/app/components/my-trades/dialogs/seller-committed/seller-committed.dialog.ts
@@ -1,5 +1,5 @@
-import { Component, Inject } from "@angular/core";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Component, Input } from "@angular/core";
+import { MyTradeItem } from "../my-trade-item.interface";
 
 @Component({
   selector: 'trades-seller-committed-dialog',
@@ -7,12 +7,6 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
   styleUrls: ['./seller-committed.dialog.scss'],
 })
 export class SellerCommittedDialog {
-  constructor(
-    public dialogRef: MatDialogRef<SellerCommittedDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
-  ) {}
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+  @Input() item: MyTradeItem | undefined
+  constructor() { }
 }
