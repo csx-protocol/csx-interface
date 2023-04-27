5bf033da599a68e0cce8049f67d2f9f757569d86
charliedevcode <charliedevcode@gmail.com>
2023-04-27 12:09:18 +0200

    subscribeToContractEvents func added

--- src/app/shared/chain-events.service.ts
+++ src/app/shared/chain-events.service.ts
@@ -1,43 +1,65 @@
 import { Injectable } from '@angular/core';
 import { NotificationService } from './notification.service';
+import { Subject, Subscription, filter } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChainEventsService {
 
+  private eventSubject = new Subject<any>();
+
   constructor(private notice: NotificationService) { this.notice.openSnackBar('event', 'OK');}
+
+  subscribeToAllEvents(callback: (event: any) => void): Subscription {
+    return this.eventSubject.asObservable().subscribe(callback);
+  }
+
+  subscribeToContractEvents2(callback: (event: any) => void, contractAddressFilter: string): Subscription {
+    return this.eventSubject.asObservable()
+      .pipe(filter(event => event.contractAddress === contractAddressFilter))
+      .subscribe(callback);
+  }
+
+  subscribeToContractEvents(callback: (event: any) => void, contractAddressFilter: string[]): Subscription {
+    return this.eventSubject.asObservable()
+      .pipe(filter(event => contractAddressFilter.includes(event.contractAddress)))
+      .subscribe(callback);
+  }
 
   onEvent(event: any) {
     console.log('catched event', event);
+    this.eventSubject.next(event);
 
     const status = event[1];
+    const contractAddress = event.contractAddress;
+    const data = event.data;
+
     console.log('status', status);
 
     switch (status) {
       case '0':
         // New Item
         this.notice.openSnackBar('Someone just listed a new item!', 'OK');
-        // data: ★ Butterfly Knife | Slaughter (Minimal Wear)||28830789185||225482466+EP2Wgs2R||0.10804083198308945
         break;
       case '2':
         // Item Sold
         this.notice.openSnackBar('Someone just purchased an item!', 'OK');
-        // data: ???
         break;
       default:
         break;
     }
 
-    if(status == '2' || status == 2){
+    if (status == '2' || status == 2) {
       this.notice.openSnackBar('Someone just purchased an item!', 'OK');
     } else {
-      //this.notice.openSnackBar(event, 'OK');
+      // this.notice.openSnackBar(event, 'OK');
     }
-
   }
 
   onError(error: any) {
     console.log('catched error', error);
+    this.notice.openSnackBar(error, 'OK');
   }
+
 }
