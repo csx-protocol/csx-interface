32fe3ac6b6bae300cded278941a15f30c86f57c5
charliedevcode <charliedevcode@gmail.com>
2023-04-27 11:54:25 +0200

    refferalCode fetcher added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, first } from 'rxjs/operators';
+import { ReferralService } from './shared/referral.service'; // Update the import path if necessary
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'interface';
+export class AppComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private referralService: ReferralService
+  ) { }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const root = this.route.root.firstChild;
+        if (root) {
+          root.queryParams.pipe(first()).subscribe((params) => {
+            const code = params['r'];
+            if (code) {
+              if (this.referralService.hasReferralCode()) {
+                // Update referral code if it already exists
+                this.referralService.updateReferralCode(code);
+              } else {
+                // Store the referral code in localStorage
+                this.referralService.setReferralCode(code);
+              }
+            }
+          });
+        }
+      });
+  }
 }
