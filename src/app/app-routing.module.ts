c7c3f80058ed9e0f58fdc04cb0542c10a57f59f7
charliedevcode <charliedevcode@gmail.com>
2023-04-27 11:54:15 +0200

    new redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MarketplaceComponent } from './pages/marketplace/marketplace.component';
+import { EarnComponent } from './pages/earn/earn.component';
+import { ReferralsComponent } from './pages/referrals/referrals.component';
 
 const routes: Routes = [
   { path: '', component: MarketplaceComponent },
+  { path: 'earn', component: EarnComponent },
+  { path: 'referrals', component: ReferralsComponent },
+  { path: '**', redirectTo: '' } // wildcard route
 ];
 
 @NgModule({
