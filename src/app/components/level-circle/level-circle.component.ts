b6ccd0d4449710b0e5d07d57119d8fb36a47ca1e
charliedevcode <charliedevcode@gmail.com>
2023-04-27 11:54:52 +0200

    reactOnClick impl

--- src/app/components/level-circle/level-circle.component.ts
+++ src/app/components/level-circle/level-circle.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Web3Service } from '../../shared/web3.service';
 import { environment } from 'src/environment/environment';
+import { MatDialog } from '@angular/material/dialog';
+import { LevelUpDialog } from './utils/level-up.dialog';
 
 // class UserTradeDetails {
 //   contractAddress: string;
@@ -21,7 +23,7 @@ export class LevelCircleComponent implements OnChanges {
   @Input() animation: boolean = true;
   @Input() animationDuration: number = 300;
   @Input() subtitle: string = '0x000000';
-  @Input() title: string = 'ERROR';
+  @Input() title: string = 'LOADING...';
   @Input() units: string = '';
   @Input() unitsColor: string = '#c4c7ef';
   @Input() titleColor: string = '#c4c7ef';
@@ -30,16 +32,17 @@ export class LevelCircleComponent implements OnChanges {
 
   @Input() userAddress: string = '0x000000';
   @Input() displayTradeDetails: boolean = false;
+  @Input() reactOnClick: boolean = true;
 
-  constructor(private web3Service: Web3Service) {}
+  constructor(private web3Service: Web3Service,  private dialog: MatDialog) { }
 
   firstTime: boolean = true;
   ngOnChanges(changes: SimpleChanges): void {
 
-    if(changes.hasOwnProperty('userAddress')){      
+    if (changes.hasOwnProperty('userAddress')) {
       this.getLevel();
       this.getTradeDetails();
-    } 
+    }
 
   }
 
@@ -77,7 +80,7 @@ export class LevelCircleComponent implements OnChanges {
   isInMinutes: boolean = false;
 
   getTradeDetails() {
-    if(this.displayTradeDetails){
+    if (this.displayTradeDetails) {
       this.web3Service.getUserDataFromUsers(this.userAddress).then((tradeDetails: any) => {
         // console.log('LevelCircleComponent getTradeDetails', tradeDetails);
         this.reputationNeg = tradeDetails.reputationNeg;
@@ -85,34 +88,44 @@ export class LevelCircleComponent implements OnChanges {
         this.totalTradesAsSeller = tradeDetails.totalTradesAsSeller;
         this.totalTradesAsBuyer = tradeDetails.totalTradesAsBuyer;
         this.totalTrades = parseInt(tradeDetails.totalTradesAsBuyer) + parseInt(tradeDetails.totalTradesAsSeller);
-        
-        const epochTime: number = parseInt(tradeDetails.deliveryInfo.averageDeliveryTime);       
+
+        const epochTime: number = parseInt(tradeDetails.deliveryInfo.averageDeliveryTime);
 
         //If average delivery time is less than 1 hour, display in minutes
         if (epochTime < 3600) {
           this.avgDeliveryTime = (epochTime / 60).toString();
           this.isInMinutes = true;
           return;
-        }    
+        }
         this.avgDeliveryTime = (epochTime / 60 / 60).toString();
       });
     }
   }
 
-  async onClick(){
-    const isOwner = this.userAddress == this.web3Service.webUser.address ? true : false;
-    console.log('LevelCircleComponent onClick isOwner?:', isOwner);    
-    // Open Dialog for Level Up
-    if(isOwner){
-
-      this.web3Service.getUserDataFromProfileLevel(this.userAddress).then((levels: any) => {
-         console.log('levels', levels);         
-      });
-
-      const res = await this.web3Service.levelUpProfileLevel('2000');
-      console.log(res, 'res');
-      
+  async onClick() {
+    if(this.reactOnClick){
+      const isOwner = this.userAddress == this.web3Service.webUser.address ? true : false;
+      console.log('LevelCircleComponent onClick isOwner?:', isOwner);
+      // Open Dialog for Level Up
+      if (isOwner) {
+        this.web3Service.getUserDataFromProfileLevel(this.userAddress).then((levels: any) => {
+          this.openDialog(levels);     
+        });
+      }
     }
+  }
+
+  openDialog(_data: any): void {   
+    const dialogRef = this.dialog.open(LevelUpDialog, {
+      data: _data,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      // console.log(`Dialog result: ${result}`);
+      if(result > 0){
+        this.getLevel();
+      }
+    });
   }
 
   getTrimmedAddress(address: string) {
@@ -124,7 +137,7 @@ export class LevelCircleComponent implements OnChanges {
     const colors = [
       '#2a2c52', // 1-9
       '#c0392b', // red:ish
-      '#ff6b00', // orange:ish
+      '#ff9b01', // orange:ish
       '#f1c40f', // yellow:ish
       '#27ae60', // darkGreen:ish
       '#2980b9', // blue:ish
@@ -134,16 +147,16 @@ export class LevelCircleComponent implements OnChanges {
       '#c5a880', // sand:ish
       '#a5a5a5', // darkwhite:ish
     ];
-  
+
     const colorIndex = levelGroup % colors.length;
     return colors[colorIndex];
   }
-  
+
   getOuterStrokeColor(level: number): string {
     const levelGroup = Math.floor(level / 10);
     const colors = [
       '#c0392b', // red:ish
-      '#ff6b00', // orange:ish
+      '#ff9b01', // orange:ish
       '#f1c40f', // yellow:ish
       '#27ae60', // darkGreen:ish
       '#2980b9', // blue:ish
@@ -152,74 +165,32 @@ export class LevelCircleComponent implements OnChanges {
       '#5b2c2f', // vinered:ish
       '#c5a880', // sand:ish
       '#a5a5a5', // darkwhite:ish
- 
+
       '#2a2c52', // 1-9
     ];
-  
+
     let colorIndex = levelGroup % colors.length;
-  
+
     // If the level is a multiple of 10 and even, use the previous color
     if (level % 10 === 0) {
       colorIndex = (colorIndex - 1 + colors.length) % colors.length;
     }
-  
+
     return colors[colorIndex];
   }
 
-  // getInnerStrokeColor(level: number): string {
-  //   const levelGroup = Math.floor(level / 10);
-  //   const colors = [
-  //     '#2a2c52', // 1-9
-  //     '#c0392b', // red:ish
-  //     '#d35400', // orange:ish
-  //     '#f1c40f', // yellow:ish
-  //     '#27ae60', // darkGreen:ish
-  //     '#2980b9', // blue:ish
-  //     '#8e44ad', // purple:ish
-  //     '#e84393', // pink:ish
-  //     '#641e16', // vinered:ish
-  //     '#c5a880', // sand:ish
-  //     '#a5a5a5', // darkwhite:ish
-  //   ];
-  
-  //   const colorIndex = levelGroup % colors.length;
-  //   return colors[colorIndex];
-  // }
-  
-  // getOuterStrokeColor(level: number): string {
-  //   const adjustedLevel = level + 1;
-  //   const levelGroup = Math.floor(adjustedLevel / 10);
-  //   const colors = [
-  //     '#2a2c52', // 1-9
-  //     '#c0392b', // red:ish
-  //     '#d35400', // orange:ish
-  //     '#f1c40f', // yellow:ish
-  //     '#27ae60', // darkGreen:ish
-  //     '#2980b9', // blue:ish
-  //     '#8e44ad', // purple:ish
-  //     '#e84393', // pink:ish
-  //     '#641e16', // vinered:ish
-  //     '#c5a880', // sand:ish
-  //     '#a5a5a5', // darkwhite:ish
-  //   ];
-  
-  //   const colorIndex = levelGroup % colors.length;
-  //   return colors[colorIndex];
-  // }
-  
-  
-// Calculate the percentage of the circle by the progress to each 10th level, and if the level is 0, 10, 20, 30, etc then return 100%
-getProgress(level: number): number {
-  if (level % 10 === 0) {
-    return 100;
+  // Calculate the percentage of the circle by the progress to each 10th level, and if the level is 0, 10, 20, 30, etc then return 100%
+  getProgress(level: number): number {
+    if (level % 10 === 0) {
+      return 100;
+    }
+
+    const levelGroup = Math.floor(level / 10);
+    const levelGroupProgress = level % 10;
+    const progress = levelGroupProgress * 10;
+    return progress;
   }
 
-  const levelGroup = Math.floor(level / 10);
-  const levelGroupProgress = level % 10;
-  const progress = levelGroupProgress * 10;
-  return progress;
-}
 
-  
 
 }
