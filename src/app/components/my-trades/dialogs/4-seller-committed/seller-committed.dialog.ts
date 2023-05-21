f2ceed9b330fadd94c62f2e0a21d8dfe0e02faca
charliedevcode <charliedevcode@gmail.com>
2023-05-21 14:48:58 +0200

    added dispute dialog confirm ux

--- src/app/components/my-trades/dialogs/4-seller-committed/seller-committed.dialog.ts
+++ src/app/components/my-trades/dialogs/4-seller-committed/seller-committed.dialog.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from "@angular/core";
 import { MyTradeItem } from "../my-trade-item.interface";
+import { Web3Service } from "../../../../shared/web3.service";
+import { MatDialog } from "@angular/material/dialog";
+import { OpenDisputeDialog } from "../10-open-dispute/open-dispute.dialog";
 
 @Component({
   selector: 'trades-seller-committed-dialog',
@@ -7,10 +10,29 @@ import { MyTradeItem } from "../my-trade-item.interface";
   styleUrls: ['./seller-committed.dialog.scss'],
 })
 export class SellerCommittedDialog {
-  @Input() item: MyTradeItem | undefined
-  constructor() { }
+  @Input() item: MyTradeItem | undefined;
+  constructor(private web3: Web3Service, private dialog: MatDialog) { }
 
+  isConfirming: boolean = false;
   confirmDelivery() {
-    
+    this.isConfirming = true;
+    this.web3.buyerConfirmReceived(this.item!.contractAddress).then((result) => {
+      console.log('result', result);
+      this.isConfirming = false;
+    }
+    ).catch((error) => {
+      console.log('error', error);
+      this.isConfirming = false;
+    });
+  }
+
+  openDispute(myTradeItem: MyTradeItem): void {
+    const dialogRef = this.dialog.open(OpenDisputeDialog, {
+      data: myTradeItem
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
