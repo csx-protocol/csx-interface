2932737fe558f34e59738fac7bc4b9b29f0ea30f
charliedevcode <charliedevcode@gmail.com>
2023-09-05 10:53:11 +0200

    trade routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { MarketplaceComponent } from './pages/marketplace/marketplace.component'
 import { EarnComponent } from './pages/earn/earn.component';
 import { ReferralsComponent } from './pages/referrals/referrals.component';
 import { MintEscrowComponent } from './pages/mint-escrow/mint-escrow.component';
+import { TradeComponent } from './pages/trade/trade.component';
 
 const routes: Routes = [
   { path: '', component: MarketplaceComponent },
+  { path: 'trade', component: TradeComponent },
+  { path: 'trade/:optionalParam', component: TradeComponent },
   { path: 'earn', component: EarnComponent },
   { path: 'referrals', component: ReferralsComponent },
   { path: 'mint', component: MintEscrowComponent },
