3513baf5ab3273a19d221f16f228050ee734b93b
charliedevcode <charliedevcode@gmail.com>
2023-09-06 17:02:28 +0200

    added animation & weapontype

--- src/app/pages/trade/utils/timeline/timeline.component.ts
+++ src/app/pages/trade/utils/timeline/timeline.component.ts
@@ -3,22 +3,36 @@ import { AfterViewInit, Component, Input } from '@angular/core';
 import { Web3Service } from '../../../../shared/web3.service';
 import { TradeStatus } from '../../../../components/my-trades/my-trades.component';
 import { TimelineService } from './timeline.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(620)
+      ]),
+      transition(':leave',
+        animate(0, style({ opacity: 0 }))
+      )
+    ])
+  ]
 })
 export class TimelineComponent implements AfterViewInit {
   @Input() contractAddress: string = '';
+  @Input() weaponType: string = '';
 
   events: TimelineEvent[] = [
-    { title: 'Item Listed', subTitle: 'For Sale', description: 'AK-47 is for sale.', status: TradeStatus.ForSale },
+    { title: 'Item Listed', subTitle: 'For Sale', description: `<weapon_type> is for sale.`, status: TradeStatus.ForSale },
     { title: 'Item Delisted', subTitle: 'Seller Cancelled', description: 'Not for sale anymore.', status: TradeStatus.SellerCancelled },
     { title: 'Item Purchased', subTitle: 'Buyer Committed', description: 'A wild buyer appeared!', status: TradeStatus.BuyerCommitted },
     { title: `Buyer's Remorse`, subTitle: 'Buyer Cancelled', description: 'The buyer backed out.', status: TradeStatus.BuyerCancelled },
-    { title: `Seller Accepted`, subTitle: 'Seller Accepted', description: 'The seller has accepted.', status: TradeStatus.SellerCommitted },
-    { title: `Seller Rejected`, subTitle: 'Seller Rejected', description: 'The seller backed out.', status: TradeStatus.SellerCancelledAfterBuyerCommitted },
+    { title: `Seller Accepted`, subTitle: 'Seller Committed', description: 'The seller has accepted.', status: TradeStatus.SellerCommitted },
+    { title: `Seller Rejected`, subTitle: 'Seller Cancelled', description: 'The seller backed out.', status: TradeStatus.SellerCancelledAfterBuyerCommitted },
     { title: `Item Sent`, subTitle: 'Completed', description: 'The trade has been completed.', status: TradeStatus.Completed },
     { title: `Trade Disputed`, subTitle: 'Disputed', description: 'Trade has been disputed.', status: TradeStatus.Disputed },
     { title: `Trade Resolved`, subTitle: 'Resolved', description: 'The trade has been resolveds.', status: TradeStatus.Resolved },
@@ -31,7 +45,7 @@ export class TimelineComponent implements AfterViewInit {
   constructor(private web3: Web3Service, private timelineService: TimelineService) { }
 
   ngAfterViewInit(): void {
-    console.log(`contractAddress`, this.contractAddress);
+    this.events[0].description = `${this.weaponType} is for sale.`;
 
     this.web3.callContractMethod('Trade', 'getStatusCount', [this.contractAddress], 'call').then((count: number) => {
       const eventsLength: number = count;
@@ -44,14 +58,14 @@ export class TimelineComponent implements AfterViewInit {
       statusHistory.push(this.events[0]);
 
       for (let i = 0; i < eventsLength; i++) {
-        const status = this.web3.callContractMethod('Trade', 'statusHistory', [this.contractAddress, i], 'call').then((status: any) => {
+        this.web3.callContractMethod('Trade', 'statusHistory', [this.contractAddress, i], 'call').then((status: any) => {
           //console.log(`status`, status);
           const event: TimelineEvent = this.events[status];
           statusHistory.push(event);
-        });
+        });        
       }
-      this.statusHistory = statusHistory;
 
+      this.statusHistory = statusHistory;
       this.isLoading = false;
     });
 
