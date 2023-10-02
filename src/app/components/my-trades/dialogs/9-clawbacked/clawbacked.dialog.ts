eabc35bc4ae02a261fa68b5bc136923c8829b716
charliedevcode <charliedevcode@gmail.com>
2023-10-02 15:40:51 +0200

    impl clawback dialog

--- src/app/components/my-trades/dialogs/9-clawbacked/clawbacked.dialog.ts
+++ src/app/components/my-trades/dialogs/9-clawbacked/clawbacked.dialog.ts
@@ -1,12 +1,24 @@
-import { Component, Inject, Input } from "@angular/core";
+import { AfterViewInit, Component, Inject, Input, OnDestroy } from "@angular/core";
 import { MyTradeItem } from "../my-trade-item.interface";
+import { Web3Service } from "../../../../shared/web3.service";
 
 @Component({
   selector: 'trades-clawbacked-dialog',
   templateUrl: './clawbacked.dialog.html',
   styleUrls: ['./clawbacked.dialog.scss'],
 })
-export class ClawbackedDialog {
-  @Input() item: MyTradeItem | undefined
-  constructor() { }
+export class ClawbackedDialog implements OnDestroy, AfterViewInit {
+  @Input() item: MyTradeItem | undefined;
+  clawbackReason: string = '';
+  constructor(private web3: Web3Service) { }
+
+  ngAfterViewInit(): void {
+    this.web3.callContractMethod('Trade', 'finalityResult', [this.item?.contractAddress], 'call').then((result: string) => {
+      this.clawbackReason = result;
+    });
+  }
+
+  ngOnDestroy(): void {
+    // this.item?.trade?.contractAddress
+  }
 }
