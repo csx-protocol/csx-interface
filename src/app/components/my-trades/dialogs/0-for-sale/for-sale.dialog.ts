4b73d5d6c0487f974c4e96a8d213d47bd7759f98
charliedevcode <charliedevcode@gmail.com>
2023-10-26 14:18:43 +0200

    added seller cancel functionality

--- src/app/components/my-trades/dialogs/0-for-sale/for-sale.dialog.ts
+++ src/app/components/my-trades/dialogs/0-for-sale/for-sale.dialog.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, Input } from "@angular/core";
 import { MyTradeItem } from "../my-trade-item.interface";
+import { Web3Service } from "../../../../shared/web3.service";
+import { ActionCardService } from "../../../../pages/trade/utils/action-card/action-card.service";
+import { TradeStatus } from "../../my-trades.component";
 
 @Component({
   selector: 'trades-for-sale-dialog',
@@ -7,6 +10,22 @@ import { MyTradeItem } from "../my-trade-item.interface";
   styleUrls: ['./for-sale.dialog.scss'],
 })
 export class ForSaleDialog {
-  @Input() item: MyTradeItem | undefined
-  constructor() { }
+  @Input() item: MyTradeItem | undefined;
+  isCancelling = false;
+  constructor(
+    private readonly web3: Web3Service,
+    private readonly actionCardService: ActionCardService) { }
+  cancelTrade() {
+    this.isCancelling = true;
+    //console.log('this.item?.contractAddress', this.item?.contractAddress);
+    
+    this.web3.callContractMethod('Trade', 'sellerCancel', [this.item?.contractAddress], 'send').then((result) => {
+      this.actionCardService.updateTradeStatus(TradeStatus.SellerCancelled);
+      this.item!.status = TradeStatus.SellerCancelled as string;
+      this.isCancelling = false;
+    }).catch((error) => {
+      console.log(error);
+      this.isCancelling = false;
+    });
+  }
 }
